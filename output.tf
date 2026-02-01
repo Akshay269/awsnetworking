edcf1df643f851d4fb6331c8c2168ed64e65c282
Akshay <aksrahangdale@gmail.com>
2026-02-01 05:42:33 +0000

    edit output

--- output.tf
+++ output.tf
@@ -1,6 +1,7 @@
-output "vpcid" {
- value = aws_vpc.main.id
+
+output "webserver_sg_id" {
+  value       = aws_security_group.sg-webserver.id
 }
-output "subnetid" {
- value = aws_subnet.main.id
+output "awskeyname" {
+ value = aws_key_pair.deployer.key_name
 }
